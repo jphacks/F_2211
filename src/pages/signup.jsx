f564cd0b27646d3ccd013a340d66652feb788cf1
higuchimmy <higutomo.0814.xr@icloud.com>
2022-10-20 15:32:40 +0900

    [fix] useRouterのimportを修正

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,5 +1,6 @@
 import { auth } from "../lib/clientApp";
-import { useState, useRouter } from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUp = () => {
