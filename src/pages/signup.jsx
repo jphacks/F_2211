300750120d5295b9b1da0e6e017f57416801bf5a
higuchimmy <higutomo.0814.xr@icloud.com>
2022-10-20 15:27:31 +0900

    [add] signupの処理を実装

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,53 @@
+import { auth } from "../lib/clientApp";
+import { useState, useRouter } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then(() => {
+        console.log("user created");
+        router.push("/");
+      })
+      .catch((error) => {
+        alert("signup失敗");
+        console.error(error);
+      });
+  };
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const handleChangePassword = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
   return (
     <>
       <p>SignUp</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={(event) => handleChangeEmail(event)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(event) => handleChangePassword(event)}
+        />
+        <button>登録</button>
+      </form>
     </>
   );
 };
